06dffe3d11a192bdb22dcbb0b1a67206b7090575
Stef Pinto <stefanopinto1@gmail.com>
2019-08-13 09:21:53 -0400

    Hacking now properly functioning
    By disabling the entire hacking functionality script after a player successfully hacks, player is no longer locked into a state of endless idle due to missed flags.

--- exit mask/Assets/Scripts/Misc/Computer.cs
+++ exit mask/Assets/Scripts/Misc/Computer.cs
@@ -16,7 +16,7 @@ public class Computer : MonoBehaviour
     {
         _animator = Player.GetComponent<Animator>();
         _collider = gameObject.GetComponent<BoxCollider>();
-        hackCoroutine = StopHacking(1);
+        hackCoroutine = StopHacking(1); // this variable represents a 1 second delay between allowing the player to hack/not hack
     }
 
     // Update is called once per frame
@@ -38,24 +38,27 @@ public class Computer : MonoBehaviour
 
     private void OnTriggerExit(Collider collision)
     {
-        if (collision.tag == "Player")
-        {
             hackingAllowed = false;
-        }
     }
 
     void Hack()
     {
         _animator.SetBool("can_hack", true);
-        Destroy(_collider); // this ensures that the player cannot hack the same computer 5 times and exit the maze very easily. 
-        StartCoroutine(hackCoroutine);
+         // this ensures that the player cannot hack the same computer 5 times and exit the maze very easily.
+         if (_collider.enabled == true)
+        {
+            _collider.isTrigger = false;
+            _collider.enabled = false;
+        }
+        StartCoroutine(hackCoroutine); // call this coroutine once the player has hacked the computer
 
         //TODO: call decrement function of door 
     }
 
     IEnumerator StopHacking(int waitTime)
     {
-        yield return new WaitForSeconds(waitTime);
+        yield return new WaitForSeconds(waitTime); // wait for the alotted time, set the hack flag to false and disable this script
         _animator.SetBool("can_hack", false);
+        this.enabled = false;
     }
 }
