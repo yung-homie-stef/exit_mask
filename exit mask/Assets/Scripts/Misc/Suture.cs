db0e17da058b02f74fc512a55dac68ec8397aab9
Stef Pinto <stefanopinto1@gmail.com>
2019-08-16 17:37:15 -0400

    Blockade logic fully functional
    Two scripts (fleshWall and Suture) have been created. The incision point, where the Suture script is applied, is a child of the blockade, which possesses its own trigger box that detects when its been sliced at with a weapon and deletes itself.  The blockade's box collider then gets disabled and plays an animation where the blockade falls apart.
    
    Tested on placeholder assets. Final materials and animations coming soon.

---
+++ exit mask/Assets/Scripts/Misc/Suture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suture : MonoBehaviour
+{
+    private GameObject _fleshWall;
+    private BoxCollider _collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = gameObject.GetComponent<BoxCollider>();
+        _fleshWall = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Weapon")
+        {
+            Destroy(gameObject);
+            _fleshWall.GetComponent<fleshWall>().RipFlesh();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ exit mask/Assets/Scripts/Misc/fleshWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fleshWall : MonoBehaviour
+{
+    private Animator _animator;
+    private BoxCollider _collider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = gameObject.GetComponent<BoxCollider>();
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void RipFlesh()
+    {
+        // _animator.SetBool("rip_flesh", true);
+        _collider.enabled = false;
+    }
+
+}
