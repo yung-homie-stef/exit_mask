faa71bbf1d08c3f8137bc442b0b5d0c289912e4b
Stef Pinto <stefanopinto1@gmail.com>
2019-08-19 18:36:46 -0400

    Added animated canvas image for when player dies and respawns
    Utilizes an animated image that lowers its alpha then brings it back to 1, simulating the player character blacking out. Responds properly to bools within its animator being prompted.

--- exit mask/Assets/Scripts/Player/Death.cs
+++ exit mask/Assets/Scripts/Player/Death.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Death : MonoBehaviour
 {
     private Animator _animator;
+    private Animator _fadeAnimator;
     private float deathTime;
     private bool deathTimerStarted;
 
     public Transform playerRespawnPoint;
     public Camera playerCamera;
+    public GameObject deathFader;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = gameObject.GetComponent<Animator>();
+        _fadeAnimator = deathFader.GetComponent<Animator>();
         deathTime = 3.0f;
         deathTimerStarted = false;
     }
@@ -35,6 +38,7 @@ public class Death : MonoBehaviour
         // play animation of player falling over
         // restrict them from moving themselves or camera
         _animator.SetBool("is_dead", true);
+        _fadeAnimator.SetBool("has_died", true);
         gameObject.GetComponent<characterController>().enabled = false;
         playerCamera.GetComponent<camMouseLook>().enabled = false;
         deathTimerStarted = true; // start death timer, making the player respawn after it has reached 0
@@ -47,6 +51,7 @@ public class Death : MonoBehaviour
         // bring the player back to life
         // respawn them at the start of the level, enable controls again, and reset the death timer
         _animator.SetBool("is_dead", false);
+        _fadeAnimator.SetBool("has_died", false);
         gameObject.GetComponent<characterController>().enabled = true;
         playerCamera.GetComponent<camMouseLook>().enabled = true;
         gameObject.transform.position = playerRespawnPoint.position;
