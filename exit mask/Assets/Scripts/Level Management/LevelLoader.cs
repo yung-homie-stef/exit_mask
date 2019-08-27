01467f9d726d84dde84391d0c13968d1ce1342f5
Stef Pinto <stefanopinto1@gmail.com>
2019-08-27 18:32:42 -0400

    Created script to attach to trigger boxes that loads the next level when hit
    Used for end-level objectives to transport the player between scenes.

---
+++ exit mask/Assets/Scripts/Level Management/LevelLoader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public GameObject image;
+    bool levelTimerSet;
+    float levelTimer;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = image.GetComponent<Animator>();
+        levelTimerSet = false;
+        levelTimer = 3.0f;
+        
+    }
+
+    private void Update()
+    {
+        if (levelTimerSet == true)
+            levelTimer -= Time.deltaTime;
+
+        if (levelTimer <= 0)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _animator.SetBool("level_completed", true);
+            levelTimerSet = true;
+
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
