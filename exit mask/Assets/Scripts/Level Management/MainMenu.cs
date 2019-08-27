2ad412c459ea133c69242fbf0e323d52c36ae7e7
Stef Pinto <stefanopinto1@gmail.com>
2019-08-27 19:36:59 -0400

    Replaced main menu screen with better one

--- exit mask/Assets/Scripts/Level Management/MainMenu.cs
+++ exit mask/Assets/Scripts/Level Management/MainMenu.cs
@@ -3,19 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public Animator animator;
-
-    public void FadeToLevel()
-    {
-        animator.SetTrigger("Fade_Out");
-        
-    }
-
-    public void OnFadeComplete()
-    {
-        PlayGame();
-    }
-
     public void PlayGame()
     {
         // this advances the current scene in the game to the next on the queue
