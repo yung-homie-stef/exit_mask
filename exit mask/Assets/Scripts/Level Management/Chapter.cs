db140c8738bd13149d0715597ebdf5a7d6717442
Stef Pinto <stefanopinto1@gmail.com>
2019-08-31 14:23:46 -0400

    Added transition scene between start menu and basement for Chapter 1
    Text needs to change though.

---
+++ exit mask/Assets/Scripts/Level Management/Chapter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Chapter : MonoBehaviour
+{
+    public GameObject image;
+    
+    private Animator _animator;
+    private IEnumerator loadCoroutine;
+    private IEnumerator fadeCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = image.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        loadCoroutine = loadNextLevel(7.0f);
+        fadeCoroutine = fadeChapterScreen(4.0f);
+        StartCoroutine(fadeCoroutine);
+        StartCoroutine(loadCoroutine);
+    }
+
+    private IEnumerator loadNextLevel(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private IEnumerator fadeChapterScreen(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        _animator.SetBool("level_completed", true);
+    }
+
+}
