f30493827b956f40778247b2743a06dc18c56444
yung-homie-stef <stefanopinto1@gmail.com>
2019-09-04 17:08:15 -0400

    Camera now follows player's bodily movements and is clamped
    Thanks Nico.

--- exit mask/Assets/Scripts/Player/CameraController.cs
+++ exit mask/Assets/Scripts/Player/CameraController.cs
@@ -10,25 +10,47 @@ public class CameraController : MonoBehaviour
     public float smoothing = 2.0f;
 
     GameObject character;
-
+    public GameObject ObjectToFollow;
+    
+    public float maxHeadRotation = 80.0f;
+    public float minHeadRotation = -80.0f;
+    
+    public float currentHeadRotation = 0;
     // Start is called before the first frame update
     void Start()
     {
         character = this.transform.parent.gameObject;
+        transform.position = ObjectToFollow.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateCamPosition();
+        //get where the mouse is moving
         var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
+        //make it slower or faster
         md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, minHeadRotation, maxHeadRotation);
+        currentHeadRotation = mouseLook.y;
+
+        //lerping
         smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
         smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
-        mouseLook += smoothV;
 
+
+
+        //jimmy neutron style 
+        mouseLook += smoothV;
         transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+
+        // how much you rotate -- what you rotate
         character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
     }
-
+    void UpdateCamPosition()
+    {
+        transform.position = ObjectToFollow.transform.position;
+    }
 }
