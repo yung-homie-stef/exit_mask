4f6eb6e576e274999b52aa72a87cc2b35404498e
Stef Pinto <stefanopinto1@gmail.com>
2019-09-08 22:35:19 -0400

    Start menu is fully restored and works in tandem with the end (thank you for playing) screen
    Try not to forget an event system for OnClick events next time....

---
+++ exit mask/Assets/Scripts/Level Management/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("Scenes/First Release Scenes/Start");
+    }
+}
