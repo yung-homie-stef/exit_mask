6115741359d077c2d50b61a8d2c6dffcf5cdccf3
yung-homie-stef <stefanopinto1@gmail.com>
2019-09-25 17:46:18 -0400

    Wheelchair enemies now play alert animations when their raycast is hit
    There is currently no way to get the wheelchair enemies to revert back to their idles after they are done pointing though. Because the alert animation plays when a flag is triggered from a ray hit, I'm not sure how to set that variable back to false since the ray is permanently "has been hit".

--- exit mask/Assets/Scripts/AI/Raycasting.cs
+++ exit mask/Assets/Scripts/AI/Raycasting.cs
@@ -6,16 +6,17 @@ public class Raycasting : MonoBehaviour
 {
     private Ray _ray;
     private RaycastHit _hit;
+    private Animator _animator;
+    private IEnumerator resetCoroutine;
 
     public Color rayColor;
     public GameObject[] cowls;
 
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = gameObject.GetComponent<Animator>();
+        resetCoroutine = ResetAlertStatus(6.0f);
     }
 
     // Update is called once per frame
@@ -24,7 +25,7 @@ public class Raycasting : MonoBehaviour
         _ray = new Ray(transform.position, transform.forward * 100);
         Debug.DrawRay(transform.position, transform.forward * 100, rayColor);
 
-        if (Physics.Raycast(transform.position, transform.forward * 100, out _hit, Mathf.Infinity))
+        if (Physics.Raycast(transform.position, transform.forward * 100, out _hit, Mathf.Infinity)) // occurs when the raycast is hit
         {
             foreach (GameObject cowl in cowls)
             {
@@ -32,7 +33,16 @@ public class Raycasting : MonoBehaviour
                 cowl.GetComponent<Animator>().SetBool("is_chasing", true);
             }
 
+
+            _animator.SetBool("is_alerted", true); // when the raycast is hit play the alert animation
+            StartCoroutine(resetCoroutine);
             Debug.Log("ray has hit");
         }
+    }
+
+    IEnumerator ResetAlertStatus(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        _animator.SetBool("is_alerted", false);
     }
 }
