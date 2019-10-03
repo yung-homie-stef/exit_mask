6b66b1df8497bac719f0b0e9f32a4ee2e9903633
yung-homie-stef <stefanopinto1@gmail.com>
2019-10-02 20:20:46 -0400

    Alerted image actions do respond properly when triggered by dev test tools
    When the bool that allows the alert canvas to appear is flagged as true, everything does work properly. Only thing needed now is to have a script attached to wheelchair enemies (involving raycasts) that sets that bool to true when alerted.

---
+++ exit mask/Assets/Scripts/Misc/alertImages.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alertImages : MonoBehaviour
+{
+    private int canvasNum;
+
+    public bool hasBeenAlerted;
+    public float alertTimer;
+    public GameObject[] images;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasBeenAlerted = false;
+        canvasNum = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TO-DO: get rid of this debug code and instead set the alerted bool to true 
+        // from another script
+        if (Input.GetKey(KeyCode.K))
+        {
+            SelectRandomCanvas(0, images.Length);
+            hasBeenAlerted = true;
+        }
+
+        if (hasBeenAlerted == true)
+        {
+            images[canvasNum].SetActive(true);
+            alertTimer -= Time.deltaTime;
+
+                if (alertTimer <= 0)
+            {
+                images[canvasNum].SetActive(false);
+                this.enabled = false;
+            }
+        }
+    }
+
+    public void SelectRandomCanvas(int min, int max)
+    {
+        canvasNum = Random.Range(min, max);
+
+    }
+}
