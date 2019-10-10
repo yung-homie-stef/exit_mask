2dad7f9e68748353e7bf147247a7e92c5a3ecdb1
yung-homie-stef <stefanopinto1@gmail.com>
2019-10-10 15:33:49 -0400

    Created a translate script for objects to move between two positions without hardcoded animations
    Thanks Remi.

---
+++ exit mask/Assets/Scripts/AI/Translate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate : MonoBehaviour
+{
+    [Header("Bounce Settings")]
+    public float length = 1;
+    public Vector3 direction = Vector3.forward;
+    public Transform first;
+    public Transform second;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        first.position = this.transform.position;
+        direction.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        second.position = this.transform.position;
+        second.position = first.position + direction * (Mathf.Sin(Time.time * speed) * length);
+        this.transform.position = second.position;
+    }
+}
