5f13b06b933b6aa4782f04db3071b6522be7fd2a
yung-homie-stef <stefanopinto1@gmail.com>
2019-11-05 12:48:40 -0500

    Created script for planned Judicator enemies

---
+++ exit mask/Assets/Scripts/AI/Judicator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Judicator : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public bool isFollowing;
+
+    public GameObject Player;
+
+    private void Start()
+    {
+        _agent = gameObject.GetComponent<NavMeshAgent>();
+        isFollowing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollowing == true) // begin following the player once their door has been unlocked, triggering this flag
+        {
+            Vector3 towardsPlayer = transform.position - Player.transform.position;
+            Vector3 newPosition = transform.position - towardsPlayer;
+            _agent.SetDestination(newPosition);
+        }
+    }
+}
