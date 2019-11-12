f3635a28b80154915d01f38309bc42f2d2a1e651
Shane Cote <cote0260@algonquinlive.com>
2019-11-12 15:10:56 -0500

    Created script that regulates rotation of enemies for adjustable patrolling behaviours

---
+++ exit mask/Assets/Scripts/AI/PatrolRotate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolRotate : MonoBehaviour
+{
+    public float dir;
+    public float currentAngle;
+    public float smallerAngle;
+    public float largerAngle;
+    public float speed;
+
+    private void Start()
+    {
+        dir = -1.0f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (dir == 1.0f)
+        {
+            currentAngle += dir * Time.deltaTime * speed;
+            gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, currentAngle, gameObject.transform.rotation.z);
+            if (currentAngle > largerAngle)
+            {
+                dir = -1.0f;
+            }
+        }
+        if (dir == -1.0f)
+        {
+            currentAngle += dir * Time.deltaTime * speed;
+            gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x, currentAngle, gameObject.transform.rotation.z);
+            if (currentAngle < smallerAngle)
+            {
+                dir = 1.0f;
+            }
+        }
+    }
+
+}
