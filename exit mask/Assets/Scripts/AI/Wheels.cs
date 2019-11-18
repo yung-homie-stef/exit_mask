06d1a5a0976a280619a466b16608e6059a4a42ba
Shane Cote <cote0260@algonquinlive.com>
2019-11-18 13:50:20 -0500

    Created line renderers to serve as visual representations for their raycasts
    Thanks Remi.

--- exit mask/Assets/Scripts/AI/Wheels.cs
+++ exit mask/Assets/Scripts/AI/Wheels.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Wheels : MonoBehaviour
 {
     private Animator _animator;
@@ -14,6 +15,9 @@ public class Wheels : MonoBehaviour
     public GameObject[] cowls;
     public GameObject staticCanvas;
 
+    private LineRenderer line;
+    public Transform headPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +26,19 @@ public class Wheels : MonoBehaviour
         rayHasHit = false;
         _animator = gameObject.GetComponent<Animator>();
         unAlertCoroutine = SetAlertToFalse(5.0f);
+
+        line = GetComponent<LineRenderer>();
+        line.startWidth = 0.05f;
+        line.endWidth = 0.05f;
+        line.startColor = Color.white;
+        line.endColor = Color.white;
+        line.numCapVertices = 6;
+
+        if(!headPosition)
+        {
+            Debug.Log(transform.name + " No head position defined.");
+            Debug.Break();
+        }
     }
 
     // Update is called once per frame
@@ -31,7 +48,7 @@ public class Wheels : MonoBehaviour
         Ray _ray = new Ray(transform.position, transform.forward);
         Debug.DrawRay(transform.position, transform.forward * rayDist, Color.green);
 
-        if (Physics.Raycast(_ray, out _hit, 100, 1 << 11)) // 11th element being the player layer
+        if (Physics.Raycast(_ray, out _hit, rayDist))
         {
             if (_hit.collider.tag == "Player" && _animator.GetBool("is_alerted") == false && rayHasHit == false)
             {
@@ -68,7 +85,8 @@ public class Wheels : MonoBehaviour
             }
         }
 
-
+        line.SetPosition(0, headPosition.position + transform.forward * 0.5f);
+        line.SetPosition(1, (headPosition.position + transform.forward * rayDist));
     }
 
     private IEnumerator SetAlertToFalse(float waitTime)
