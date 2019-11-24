814f00be325a7fa49789d6f33c5b28ff390b0a5b
yung-homie-stef <stefanopinto1@gmail.com>
2019-11-24 14:06:00 -0500

    Created a script that measures distance between player and third chapter objectives
    Script is supposed to alter the alpha of an on-screen image once the minimum distance between objective and player has been breached but trying to get the inverse lerp function to function properly is still a work in progress.

---
+++ exit mask/Assets/Scripts/Misc/Guidance.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Guidance : MonoBehaviour
+{
+    public GameObject hintImage;
+    public GameObject Player;
+    public GameObject Wires;
+    public float minimumDistance;
+        
+    private Color imageColour;
+    private bool activatingAllowed;
+    private bool activateImage;
+    private float distanceBetween;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageColour = hintImage.GetComponent<Image>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceBetween = Vector3.Distance(Player.transform.position, gameObject.transform.position);
+
+        if (distanceBetween < minimumDistance)
+        {
+            hintImage.SetActive(true);
+        }
+        else
+            hintImage.SetActive(false);
+
+        if (activatingAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            Activate();
+        }
+
+        if (activateImage)
+            hintImage.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            activatingAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        activatingAllowed = false;
+    }
+
+    private void Activate()
+    {
+        Wires.SetActive(true);
+        Destroy(hintImage);
+    }
+}
