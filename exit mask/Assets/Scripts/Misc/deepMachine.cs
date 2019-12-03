7217e700b41a858f2258ca803be9d03b3c109a54
yung-homie-stef <stefanopinto1@gmail.com>
2019-12-02 23:25:23 -0500

    Decoupled incubator objective and objective based respawning scripts
    Also placed the judicator in the fifth layer of the deep.

---
+++ exit mask/Assets/Scripts/Misc/deepMachine.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class deepMachine : MonoBehaviour
+{
+    private BoxCollider _collider;
+    private bool unlockingAllowed = false;
+    private IEnumerator prisonCoroutine;
+    private Transform newRespawnPosition;
+    private bool canIncrement = true;
+    private GameObject[] judicators;
+    private bool isDead = false;
+    private float judicatorRepositioningTimer = 3.0f;
+
+    public GameObject Judicator;
+    public GameObject Player;
+
+    public GameObject[] prisonBars;
+    public GameObject[] prisonGates;
+    public GameObject flyScreen;
+    public Transform judicatorTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = gameObject.GetComponent<BoxCollider>();
+        prisonCoroutine = DestroyPrisonBars(3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(unlockingAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            Unlock();
+        }
+
+        if (isDead)
+            judicatorRepositioningTimer -= Time.deltaTime;
+
+        if (judicatorRepositioningTimer <= 0)
+        {
+            foreach (GameObject jude in judicators)
+            {
+                jude.transform.position = jude.GetComponent<Judicator>().judicatorTransform.position;
+                jude.GetComponent<NavMeshAgent>().ResetPath();
+            }
+
+            judicatorRepositioningTimer = 3.0f;
+            isDead = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            unlockingAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        unlockingAllowed = false;   
+    }
+
+    private void Unlock()
+    {
+        canIncrement = true;
+
+        //FindObjectOfType<audioManager>().Play("Heart_Machine");
+
+        // activate the chasing capabilities of the judicator
+        Judicator.GetComponent<Judicator>().isFollowing = true;
+        Judicator.GetComponent<Animator>().SetBool("is_following", true);
+
+        //activate fly screens
+        flyScreen.SetActive(true);
+
+        #region Respawning
+        // set the respawn position to the position of the player when they unlock a Judicator
+        newRespawnPosition = Player.transform;
+        Player.GetComponent<Death>().playerRespawnPoint.position = newRespawnPosition.position;
+        Player.GetComponent<objectiveBasedRespawning_2>().machinesList.Add(this.gameObject);
+        Debug.Log(Player.GetComponent<objectiveBasedRespawning_2>().machinesList.Count);
+        #endregion
+
+        #region Prison
+        // get animator of the bars and play unlocking animation
+        for (int i = 0; i < prisonBars.Length; i++)
+        {
+            if (prisonBars[i].GetComponent<Animator>().GetBool("is_unlocked") == false)
+            {
+                prisonBars[i].GetComponent<Animator>().SetBool("is_unlocked", true);
+                prisonBars[i].GetComponent<Animator>().SetBool("is_locked", false);
+            }
+        }
+
+        // get animator of the bars and play locking animation
+        for (int i =0; i < prisonGates.Length; i++)
+        {
+            prisonGates[i].SetActive(true);
+        }
+
+        this.enabled = false;
+        #endregion
+    }
+
+    private IEnumerator DestroyPrisonBars(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        for (int i = 0; i < prisonBars.Length; i++)
+        {
+            prisonBars[i].SetActive(false);
+        }
+    }
+
+    public void ResetJudicatorStatus()
+    {
+
+        // get animator of the bars and play locking animation
+        for (int i = 0; i < prisonBars.Length; i++)
+        {
+            prisonBars[i].GetComponent<Animator>().SetBool("is_locked", true);
+            prisonBars[i].GetComponent<Animator>().SetBool("is_unlocked", false);
+        }
+
+        // get animator of the bars and play locking animation
+        for (int i = 0; i < prisonGates.Length; i++)
+        {
+            prisonGates[i].SetActive(false);
+        }
+
+        // put the judicator back in the cage and turn off the fly screen
+        Judicator.GetComponent<Judicator>().isFollowing = false;
+        Judicator.GetComponent<Animator>().SetBool("is_following", false);
+
+        judicators = GameObject.FindGameObjectsWithTag("Judicator_Enemy");
+
+        isDead = true;
+
+        flyScreen.SetActive(false);
+        this.enabled = true;
+
+        if (canIncrement == true)
+        {
+            canIncrement = false;
+        }
+    }
+}
---
+++ exit mask/Assets/Scripts/Player/objectiveBasedRespawning_2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class objectiveBasedRespawning_2 : MonoBehaviour
+{
+    private GameObject Player;
+
+    public List<GameObject> machinesList = new List<GameObject>();
+    public GameObject firstSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        machinesList.Add(firstSpawn);
+        Player = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.GetComponent<Death>().deathTimerStarted == true)
+        { 
+            Debug.Log("lmao u died");
+            // to ensure this only happens once at least one transfusion machine is active
+            if (machinesList.Count > 1)
+            {
+                machinesList[machinesList.Count - 1].GetComponent<deepMachine>().ResetJudicatorStatus();
+
+             
+            }
+
+                
+        }
+    }
+}
