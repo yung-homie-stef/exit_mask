5e59c0a3d700403e8b154d4ad5697a3e373cf6b8
Stef Pinto <stefanopinto1@gmail.com>
2019-12-04 14:23:21 -0500

    Added final boss with death animation and second box cutter to pick up in home scene

---
+++ exit mask/Assets/Scripts/AI/FinalBoss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBoss : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Weapon")
+        {
+            //play kill animation
+            //play death sound 
+            _animator.SetBool("is_killed", true);
+        }
+    }
+}
