dabde2cb3c6dc4aaa5a6c6345614f96909792fa7
yung-homie-stef <stefanopinto1@gmail.com>
2019-12-05 22:17:39 -0500

    Refined transition after killing final boss
    Last screen in game still needs to transition to main menu after 10 or so seconds.

--- exit mask/Assets/Scripts/AI/FinalBoss.cs
+++ exit mask/Assets/Scripts/AI/FinalBoss.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinalBoss : MonoBehaviour
 {
     public Animator textAnimator;
+    public GameObject levelFader;
 
     private Animator _animator;
     private IEnumerator centipedeCoroutine;
+    private IEnumerator fadeScreen;
+    private bool timerRunning;
+    private float timer = 14.0f;
+    private Animator _fadeAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = gameObject.GetComponent<Animator>();
+        _fadeAnimator = levelFader.GetComponent<Animator>();
         centipedeCoroutine = Kill(0.25f);
+        fadeScreen = BeginFadingScreen(10.0f);
+        timerRunning = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,11 +33,39 @@ public class FinalBoss : MonoBehaviour
         }
     }
 
-     IEnumerator Kill(float waitTime)
+    private IEnumerator BeginFadingScreen(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        _fadeAnimator.SetBool("level_completed", true);
+    }
+
+    private void Update()
+    {
+        if (timerRunning == true)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            LoadLevel();
+        }
+    }
+
+    IEnumerator Kill(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
         _animator.SetBool("is_killed", true);
         textAnimator.SetBool("picked_up", true);
+        timerRunning = true;
+        StartCoroutine(fadeScreen);
+
         //play death sound 
     }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
 }
